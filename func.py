5fe5ac8883bfb65e8f00a840fc67943ec210ae58
maxfrol96 <maxfrol96@mail.ru>
2023-01-26 21:24:22 +0300

    Добавление функции перевода первой буквы в верхний регистр + комменты

--- func.py
+++ func.py
@@ -1,5 +1,13 @@
 def words_up(word):
+    """
+    Ну что-то произойдет
+    """
     return word.upper()
 
 print(words_up('asd'))
 
+def words_first_up(word):
+    """
+        Тут вроде тоже
+    """
+    return word.capitalize()
